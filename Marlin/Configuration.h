fb1b796c8814b99b4491cce274a5b27953186bdb
Lee <leland.fried@engent.com>
2025-01-06 09:17:12 -0800

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1715,11 +1715,11 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
+#define X_BED_SIZE 175
 #if ENABLED(LARGE_BED)
   #define Y_BED_SIZE 280
 #else
-  #define Y_BED_SIZE 200
+  #define Y_BED_SIZE 190
 #endif
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
